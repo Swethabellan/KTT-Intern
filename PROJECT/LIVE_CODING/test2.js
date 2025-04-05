84dd4d06e4dccf291ca4acbc49e9d09b9d39bd9e
Swethabellan <swethabellan@gmail.com>
2025-04-05 18:44:09 +0530

    ktt-live_coding

--- PROJECT/LIVE_CODING/test2.js
+++ PROJECT/LIVE_CODING/test2.js
@@ -9,16 +9,41 @@
 // product.finalPrice=(product.price*product.discount)/100;
 // product;
 
-let product = {
-    id: 101,
-    name: "Bag",
-    price: 500,
-    discount: 10,
-    finalPrice:function() {
-        // const discountAmount = (this.price * this.discount) / 100;
-        return this.price - (this.price *this.discount)/100;
+// let product = {
+//     id: 101,
+//     name: "Bag",
+//     price: 500,
+//     discount: 10,
+//     finalPrice:function() {
+//         // const discountAmount = (this.price * this.discount) / 100;
+//         return this.price - (this.price *this.discount)/100;
+//     }
+// };
+// product.finalPrice1=product.finalPrice();
+// delete product.finalPrice;
+// console.log(product);
+
+let a=[1,2,3];
+let b=[];
+a.pop();
+console.log(a);
+console.log(a.length);
+delete a[1];
+a[5]=12;
+a[100]=123;
+console.log(a);
+console.log(a.length);
+// delete a[1];
+delete a[100];
+// console.log(a.length);
+for(let val in a){
+    if(val==="")
+        continue;
+    else{
+    let b=a.val;
+    // console.log(a.length);
     }
-};
-product.finalPrice1=product.finalPrice();
-delete product.finalPrice;
-console.log(product);
+        
+}
+console.log(b.length);
+console.log(a.length);
