a3c8c78c39cc2fda72c80ac049b2b317d3442019
Swethabellan <swethabellan@gmail.com>
2025-04-15 19:31:39 +0530

    correction

--- LEARNINGS/Node.js/Myfirst.js
+++ LEARNINGS/Node.js/Myfirst.js
@@ -1,39 +1,39 @@
-// const http = require('http');
-
-// const server = http.createServer((req, res) => {
-//   res.statusCode = 200;
-//   res.setHeader('Content-Type', 'text/plain');
-//   res.end('Hi SwethaBellan\n');
-// });
-
-// const PORT = 3000;
-// server.listen(PORT, () => {
-//   console.log(`Server running at http://localhost:${PORT}/`);
-// });
-
 const http = require('http');
-const fs = require('fs');
 
 const server = http.createServer((req, res) => {
-  if (req.url === '/file' && req.method === 'GET') {
-    fs.readFile('example.txt', 'utf8', (err, data) => {
-      if (err) {
-        res.statusCode = 500;
-        res.setHeader('Content-Type', 'text/plain');
-        res.end('Error reading file\n');
-        return;
-      }
-      res.statusCode = 200;
-      res.setHeader('Content-Type', 'text/plain');
-      res.end(data);
-    });
-  } else {
-    res.statusCode = 404;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('404: Not Found\n');
-  }
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hi SwethaBellan\n');
 });
 
-server.listen(3000, () => {
-  console.log('Server running at http://localhost:3000/');
+const PORT = 3000;
+server.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}/`);
 });
+
+const http = require('http');
+// const fs = require('fs');
+
+// const server1 = http.createServer((req, res) => {
+//   if (req.url === '/file' && req.method === 'GET') {
+//     fs.readFile('example.txt', 'utf8', (err, data) => {
+//       if (err) {
+//         res.statusCode = 500;
+//         res.setHeader('Content-Type', 'text/plain');
+//         res.end('Error reading file\n');
+//         return;
+//       }
+//       res.statusCode = 200;
+//       res.setHeader('Content-Type', 'text/plain');
+//       res.end(data);
+//     });
+//   } else {
+//     res.statusCode = 404;
+//     res.setHeader('Content-Type', 'text/plain');
+//     res.end('404: Not Found\n');
+//   }
+// });
+
+// server.listen(3000, () => {
+//   console.log('Server running at http://localhost:3000/');
+// });
