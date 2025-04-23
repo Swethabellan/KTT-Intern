6503d5f93850c80a925049dd8b60a91008e33464
Swethabellan <swethabellan@gmail.com>
2025-04-23 18:25:39 +0530

    database_file updation

--- PROJECT/Techtalk/backend/config/database.js
+++ PROJECT/Techtalk/backend/config/database.js
@@ -3,6 +3,7 @@ const {Sequelize} = require("sequelize");
 const sequelize = new Sequelize("Techtalk", "postgres", "SwethaBellan", {
     host : 'localhost',
     dialect: 'postgres',
+    logging:false
 });
 
 module.exports = sequelize;
