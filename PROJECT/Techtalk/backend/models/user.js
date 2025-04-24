641e39c949b8a53b413e806784731507d0977256
Swethabellan <swethabellan@gmail.com>
2025-04-24 18:44:30 +0530

    user model updated

--- PROJECT/Techtalk/backend/models/user.js
+++ PROJECT/Techtalk/backend/models/user.js
@@ -2,30 +2,38 @@ const { DataTypes } = require('sequelize');
 const sequelize = require('../config/database');
 
 const User = sequelize.define('User', {
-  username: { 
+  username: {
     type: DataTypes.STRING,
-     allowNull: false, 
-     unique: true },
-  email: { 
-    type: DataTypes.STRING, 
     allowNull: false,
-     unique: true },
-  password: { 
+    unique: true
+  },
+  email: {
     type: DataTypes.STRING,
-     allowNull: false },
+    allowNull: false,
+    unique: true
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
   name: {
-     type: DataTypes.STRING,
-      allowNull: false },
+    type: DataTypes.STRING,
+    allowNull: false
+  },
   role: {
-     type: DataTypes.STRING,
-      allowNull: false },
-  team: {
-     type: DataTypes.STRING,
-      allowNull: false },
-  bio: { 
-    type: DataTypes.TEXT },
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  bio: {
+    type: DataTypes.TEXT,
+    allowNull: true,
+    defaultValue: ''
+  },
   skills: {
-     type: DataTypes.STRING }
+    type: DataTypes.STRING,
+    allowNull: true,
+    defaultValue: ''
+  }
 }, { timestamps: false });
 
 module.exports = User;
