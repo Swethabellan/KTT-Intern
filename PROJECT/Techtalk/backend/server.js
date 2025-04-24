b7d4009897dc9f33d1d3e075fd60f217d34514e8
Swethabellan <swethabellan@gmail.com>
2025-04-24 18:42:53 +0530

    added routes and relations

--- PROJECT/Techtalk/backend/server.js
+++ PROJECT/Techtalk/backend/server.js
@@ -5,15 +5,33 @@ const cors = require('cors');
 const { Sequelize, DataTypes, Op } = require('sequelize');
 const bcrypt = require('bcryptjs');
 const userRoutes=require('./router/Userroute')
+const questionRoutes=require('./router/questionroute');
+ 
+const User =require('./models/user')
+const Questions=require('./models/questions');
+const Answers=require('./models/answer');
+const Profile = require('./models/profile');
 
 const sequelize = require('./config/database');
 const app=express()
 const PORT = 3000;
 
+User.hasMany(Questions, { foreignKey: 'userId' });
+Questions.belongsTo(User, { foreignKey: 'userId' });
+User.hasMany(Answers, { foreignKey: 'userId' });
+Questions.hasMany(Answers, { foreignKey: 'questionId' });
+Answers.belongsTo(Questions, { foreignKey: 'questionId' });
+
+User.hasOne(Profile, { foreignKey: 'userId' });
+Profile.belongsTo(User, { foreignKey: 'userId' });
 sequelize.sync();
 
+app.use(express.json()); 
+app.use(cors());
+
 app.use('/api', userRoutes);
-// app.use('/api',questionroute);
+app.use('/api',questionRoutes);
+
 
 app.listen(PORT, () => {
     console.log(`App is running at port ${PORT}`);
