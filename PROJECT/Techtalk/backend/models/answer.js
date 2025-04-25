ef4a1feffc02880450e7d076282f649b0fd4e96c
Swethabellan <swethabellan@gmail.com>
2025-04-25 18:37:01 +0530

    answer model updation

--- PROJECT/Techtalk/backend/models/answer.js
+++ PROJECT/Techtalk/backend/models/answer.js
@@ -1,16 +1,19 @@
 const { DataTypes } = require('sequelize');
 const sequelize = require('../config/database');
+const User = require('./user');
+const Question = require('./question');
 
-const Answers = sequelize.define('Answers', {
-  questionId: {
-     type: DataTypes.INTEGER, 
-     allowNull: false },
-  userId: {
-     type: DataTypes.INTEGER, 
-     allowNull: false },
-  content: { 
-    type: DataTypes.TEXT, 
-    allowNull: false }
-});
+const Answer = sequelize.define('Answer', {
+  id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+  questionId: { type: DataTypes.INTEGER, allowNull: false },
+  userId: { type: DataTypes.INTEGER, allowNull: false },
+  content: { type: DataTypes.TEXT, allowNull: false }
+}, { timestamps: true });
 
-module.exports = Answers;
+User.hasMany(Answer, { foreignKey: 'userId' });
+Answer.belongsTo(User, { foreignKey: 'userId' });
+
+Question.hasMany(Answer, { foreignKey: 'questionId' });
+Answer.belongsTo(Question, { foreignKey: 'questionId' });
+
+module.exports = Answer;
