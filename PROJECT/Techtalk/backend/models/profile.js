6fb0f53b9b857dbdc41e424388b9a6708fb39b68
Swethabellan <swethabellan@gmail.com>
2025-04-25 18:37:21 +0530

    profile model updation

--- PROJECT/Techtalk/backend/models/profile.js
+++ PROJECT/Techtalk/backend/models/profile.js
@@ -1,38 +1,18 @@
 const { DataTypes } = require('sequelize');
 const sequelize = require('../config/database');
+const User = require('./user');
 
 const Profile = sequelize.define('Profile', {
-  userId: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-    unique: true, 
-    references: {
-      model: 'Users',
-      key: 'id'
-    }
-  },
-  profilePicture: {
-    type: DataTypes.STRING,
-    allowNull: true,
-    defaultValue: '' 
-  },
-  location: {
-    type: DataTypes.STRING,
-    allowNull: true,
-    defaultValue: '' 
-  },
-  github: {
-    type: DataTypes.STRING,
-    allowNull: true,
-    defaultValue: '' 
-  },
-  linkedin: {
-    type: DataTypes.STRING,
-    allowNull: true,
-    defaultValue: ''
-  }
-}, {
-  timestamps: false 
-});
+  id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+  userId: { type: DataTypes.INTEGER, allowNull: false },
+  profilePicture: { type: DataTypes.STRING, allowNull: true, defaultValue: '' },
+  location: { type: DataTypes.STRING, allowNull: true, defaultValue: '' },
+  website: { type: DataTypes.STRING, allowNull: true, defaultValue: '' },
+  github: { type: DataTypes.STRING, allowNull: true, defaultValue: '' },
+  linkedin: { type: DataTypes.STRING, allowNull: true, defaultValue: '' }
+}, { timestamps: true });
+
+User.hasOne(Profile, { foreignKey: 'userId' });
+Profile.belongsTo(User, { foreignKey: 'userId' });
 
 module.exports = Profile;
